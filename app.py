837349213f115be0c94de94029365c96f59bfd3f
Arunachalamcyber <165225563+Arunachalamcyber@users.noreply.github.com>
2025-05-31 05:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
 import pandas as pd
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    df = pd.read_csv('packets_with_anomalies.csv')
-    anomalies = df[df['anomaly'] == -1]
-    anomalies = anomalies.to_dict(orient='records')
-    return render_template('index.html', anomalies=anomalies)
+    df = pd.read_csv('packets_with_anomalies.csv')   # Data loading
+
+    print(df.columns)  # To verify columns in the console
+
+    anomalies = df[df['is_anomaly'] == True]
+
+
+    # Convert DataFrame to list of dicts (records) to pass to template
+    anomalies_records = anomalies.to_dict(orient='records')
+
+    # Pass the list of anomaly records to the template
+    return render_template('index.html', anomalies=anomalies_records)
 
 if __name__ == '__main__':
     app.run(debug=True)
